01093cc3c20383c0e9d07d3ee5cc24bf32c7c6ca
apoloa <a.whole.dev@gmail.com>
2017-05-17 13:25:24 +0200

    Added more lint and comments to export things

--- model/route.go
+++ model/route.go
@@ -1,13 +1,15 @@
 package model
 
+// Route defines the routes inside the server
 type Route struct {
-	Method string `yaml:"method"`
-	Route string `yaml:"route"`
-	Response string `yaml:"response"`
-	StatusCode int `yaml:"statusCode"`
+	Method     string `yaml:"method"`
+	Route      string `yaml:"route"`
+	Response   string `yaml:"response"`
+	StatusCode int    `yaml:"statusCode"`
 }
 
+// Server model that defines a server with the port to listen and the routes
 type Server struct {
-	Port int `yaml:"port"`
+	Port   int     `yaml:"port"`
 	Routes []Route `yaml:"routes"`
 }
